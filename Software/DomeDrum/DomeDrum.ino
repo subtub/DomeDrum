67cdcc4c6a562ee597f202a24c8112173b0de707
Paul Vollmer <wrongentertainment@yahoo.de>
2013-02-02 11:05:01 +0100

    created constants file to store all defined variables, update main sketch

--- Software/DomeDrum/DomeDrum.ino
+++ Software/DomeDrum/DomeDrum.ino
@@ -6,83 +6,59 @@
  */
 
 #include "FSR.h"
+#include "DomeDrumConstants.h"
 #include <CapSense.h>
 
-#define NUM_FSRS 10
-
-#define PIN_START 12
-#define TEENSY_LED_PIN 11
-#define BUTTON_PIN 10
-
-#define MIDI_CHANNEL 1
-#define MIDI_NOTE_START 48 // C4
-#define VELOCITY_DEF_THRESHOLD 1 // der standart thresholöd [0..127]
-
-#define CAP_BUFFER_SIZE 50
-
-FSR fsr[NUM_FSRS];
+FSR fsr[FSR_TOTAL];
 CapSense cs = CapSense(4, 6);
 
 int max = 0;
-long capBuffer[CAP_BUFFER_SIZE];
+long capBuffer[CAPSENSE_BUFFER_SIZE];
 int capBufferCounter = 0;
 
+
+/**
+ * Main setup
+ */
 void setup() {
   // declare LED as output
-  pinMode(TEENSY_LED_PIN, OUTPUT);
+  pinMode(TEENSY_PIN_LED, OUTPUT);
   // declare pushbutton as input
-  pinMode(BUTTON_PIN, INPUT);
+  pinMode(TEENSY_PIN_BUTTON, INPUT);
   
   initFsrs();
   initCapBuffer();
   
   // cap sense
   //cs.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  
+  
   Serial.begin(9600);
   Serial.println("hello");
 }
 
-long getAverage(long arr[], int arraySize){
-  long sum = 0;
-  for(int i=0; i<arraySize; i++){
-    sum += arr[i];
-  }
-  return sum / arraySize;
-} 
 
+/**
+ * Main loop
+ */
 void loop() {
-  int val = digitalRead(BUTTON_PIN);  // read input value
+  int val = digitalRead(TEENSY_PIN_BUTTON);  // read input value
   if (val == HIGH) {         // check if the input is HIGH (button released)
-    digitalWrite(TEENSY_LED_PIN, LOW);  // turn LED OFF
+    digitalWrite(TEENSY_PIN_LED, LOW);  // turn LED OFF
   } else {
-    digitalWrite(TEENSY_LED_PIN, HIGH);  // turn LED ON
+    digitalWrite(TEENSY_PIN_LED, HIGH);  // turn LED ON
   }
   
-  for(int i=0; i<NUM_FSRS; i++){
+  for(int i=0; i<FSR_TOTAL; i++){
     fsr[i].update();
   }
   
-  /*
-  for(int i=0; i<NUM_FSRS; i++) {
-    int s = analogRead(12+i);
-    usbMIDI.sendNoteOn(10, fsrToMidi(s), 1+i);
-  }
-  */
-  /*int s1 = analogRead(12);
-  usbMIDI.sendNoteOn(10, fsrToMidi(s1), 1);
-  int s2 = analogRead(13);
-  usbMIDI.sendNoteOn(10, fsrToMidi(s2), 2);
-  int s3 = analogRead(14);
-  usbMIDI.sendNoteOn(10, fsrToMidi(s3), 3);
-  int s4 = analogRead(15);
-  usbMIDI.sendNoteOn(10, fsrToMidi(s4), 4);*/
-  
   long total1 =  cs.capSense(30);
-  if(capBufferCounter >= CAP_BUFFER_SIZE) capBufferCounter = 0;
+  if(capBufferCounter >= CAPSENSE_BUFFER_SIZE) capBufferCounter = 0;
   capBuffer[capBufferCounter] = total1;
-  long avg = getAverage(capBuffer, CAP_BUFFER_SIZE);  
+  long avg = getAverage(capBuffer, CAPSENSE_BUFFER_SIZE);  
   capBufferCounter++;
-  usbMIDI.sendNoteOn(midiNote, map()getAverage, 300, , midiChannel);
+  usbMIDI.sendNoteOn(midiNote, map()getAverage, 300, midiChannel);
   Serial.println(avg);
   /*
   if(total1 > max){
@@ -105,15 +81,25 @@ void loop() {
   delay(1);
 }
 
+
+long getAverage(long arr[], int arraySize){
+  long sum = 0;
+  for(int i=0; i<arraySize; i++){
+    sum += arr[i];
+  }
+  return sum / arraySize;
+}
+
+
 void initFsrs(){
   // Wir benutzen die Teensy Pins 21 - 12
-  for(int i=0; i<NUM_FSRS; i++){
-    fsr[i].init(PIN_START+i, VELOCITY_DEF_THRESHOLD, MIDI_CHANNEL, MIDI_NOTE_START+i);
+  for(int i=0; i<FSR_TOTAL; i++){
+    fsr[i].init(TEENSY_PIN_FSR_START+i, VELOCITY_DEF_THRESHOLD, MIDI_CHANNEL, MIDI_NOTE_START+i);
   }
 }
 
 void initCapBuffer(){
-  for(int i=0; i<CAP_BUFFER_SIZE; i++){
+  for(int i=0; i<CAPSENSE_BUFFER_SIZE; i++){
     capBuffer[i] = 0;
   }
 }
---
+++ Software/DomeDrum/DomeDrumConstants.h
@@ -0,0 +1,18 @@
+/**
+ * DomeDrum Constants
+ */
+ 
+// Hardware
+#define TEENSY_PIN_LED 11
+#define TEENSY_PIN_BUTTON 10
+#define TEENSY_PIN_FSR_START 12 // We uset pin 12 to 22 
+#define FSR_TOTAL 10
+
+// MIDI
+#define MIDI_CHANNEL 1
+#define MIDI_NOTE_START 48 // C4
+
+// Software
+// TODO: rename to FSR
+#define VELOCITY_DEF_THRESHOLD 1 // der standart thresholöd [0..127]
+#define CAPSENSE_BUFFER_SIZE 50
